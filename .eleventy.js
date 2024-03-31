dd97652b365b6ec4d1f11bf8df8b79a7ad3c8b8b
lautaro <lautaroarielaraujo@gmail.com>
2024-03-30 23:54:44 -0300

    add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/css/normalize.css");
   eleventyConfig.addPassthroughCopy("src/assets/css/style.css");
+  eleventyConfig.addPassthroughCopy("src/assets/img/favicon.ico");
   return {
     htmlTemplateEngine: "njk",
     passthroughFileCopy: true,
